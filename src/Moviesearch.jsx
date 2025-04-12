a581a0536d1b6c517f98e4b8fbbef429d1973689
haakoaal <haakoaal@stud.ntnu.no>
2025-04-12 15:17:39 +1000

    clean up

--- src/Moviesearch.jsx
+++ src/Moviesearch.jsx
@@ -1,12 +1,23 @@
-import MSButton from './components/MSButton'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './styles/moviesearch.css'
+import { LoginWrapper } from './components/LoginContext'
+import Home from './routes/Home'
+import Header from './components/Header'
+
+export const API_URL = import.meta.env.VITE_API_URL;
 
 function Moviesearch() {
 
   return (
     <>
-      <h1> Moviesearch </h1>
-      <MSButton text={"Button"}/>
+      <BrowserRouter>
+        <LoginWrapper>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />}/>
+          </Routes>
+        </LoginWrapper>
+      </BrowserRouter>
     </>
   )
 }
