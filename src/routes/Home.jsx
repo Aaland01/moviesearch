cb0fd6b1a68dceed09eb75a94dc0908976c1de67
haakoaal <haakoaal@stud.ntnu.no>
2025-04-14 20:12:37 +1000

    title and hero

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,11 +4,20 @@ import LogOutButton from "../components/LogOutButton";
 const Home = () => {
   return (
     <>
-      <Container>
-        <h1> Moviesearch </h1>
+      <div className="fixed-bottom text-end p-2">
+        {/* Temporary logout button */}
+        <LogOutButton />
+      </div>
+
+      <div className="herowrapper">
+        <img src="heroimage.png" className="img-fluid"/>
+      </div>
+
+      <Container className="text-center">
+        <h1 className="title"> Moviesearch </h1>
+
+
       </Container>
-      {/* Temporary logout button */}
-      <LogOutButton />
     </>
   )
 };
