b7ef3e498b71defcd2a4e1e642ed9f124d035009
haakoaal <haakoaal@stud.ntnu.no>
2025-04-18 23:15:03 +1000

    Component for person, to adjust for protecting it

--- src/routes/People.jsx
+++ src/routes/People.jsx
@@ -1,123 +1,79 @@
 import { useEffect, useState } from "react";
 import { Container } from "reactstrap";
 import { API_URL } from "../Moviesearch";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import GridTable from "../components/GridTable";
+import { useSearchParams } from "react-router-dom";
+import Person from "../components/Person";
 
 const People = () => {
-
-  const navigate = useNavigate();
 
   const [params] = useSearchParams();
   const personURL = `${API_URL}/people/${params.get("id")}`
   
-  const token = localStorage.getItem("token")
+  const token = localStorage.getItem("bearerToken")
 
-  const [person, setPerson] = useState({})
+  const [personDetails, setPerson] = useState({})
   const [movies, setMovies] = useState([])
   const [loading, setLoading] = useState(true)
 
+  const getPersonDetails = async () => {
+    try {
+      console.log(`Fetching from: \n -> ${personURL}` )
+      const response = await fetch(personURL, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`,
+        },
+      });
+      const json = await response.json();
+      console.log(json);
+      if ( response.status === 401 ) {
+        handle401(response.message)
+      } else if (response.ok){
+        console.log("Ok, setting data");
+        setData(json);
+      } else {
+        throw new Error(json.message);
+      }
+    } catch (error) {
+      console.error("Error retrieving data", error.message)
+    }
+  }
+
+  const setData = (json) => {
+    setPerson({
+      name: json.name,
+      birthYear: json.birthYear,
+      deathYear: json.deathYear
+    });
+    setMovies(json.roles);
+    setLoading(false);
+    console.log("Data set for ", json.name);
+  }
+
   useEffect(() => {
-    console.log(`Fetching from: \n -> ${personURL}` )
-
-    fetch(personURL, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${token}`,
-      },
-    })
-      .then(async res => {
-        if ( res.status === 401 ) {
-          const errorJSON = await res.json();
-          handle401(errorJSON);
-          throw new Error("401 Unauthorized")
-          
-        } else if (res.ok){
-          return res.json();
-
-        } else {
-          const errorJSON = await res.json();
-          handleError(errorJSON.message);
-          throw new Error("Request failed")
-        }
-      })
-      .then(json => {
-        console.log(json);
-        
-        setPerson({
-          name: json.name,
-          birthYear: json.birthYear,
-          deathYear: json.deathYear
-        });
-        setMovies((json.roles));
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error when requesting person: ", error.message)
-      })
+    getPersonDetails();
   }, [])
 
-  const handle401 = (errorJSON) => {
-    // Something to redirect
+  const handle401 = (errorMessage) => {
+    // case 1
+    // Authorization header ('Bearer token') not found
+    // Code related - non-user specific
+    // case 2
+    // JWT token has expired
+    // handle refresh
+    // case 3
+    // Invalid JWT token
+    // Cannot access this
+    // handle logout if not refresh and not bearer
+    // 
   }
-  
-  const handleError = (message) => {
-    console.log(message);
-  }
-  
-  /**
-   * 
-   * 
-   * {
-      "movieName": "Star Trek: First Contact",
-      "movieId": "tt0117731",
-      "category": "actor",
-      "characters": [
-        "Picard"
-      ],
-      "imdbRating": 7.6
-    }
-   */
-
-  const columns = [
-    {headerName: "Role", field: "category"},
-    {headerName: "Movie", field: "movieName"},
-    {headerName: "Characters", field: "characters"},
-    {headerName: "IMDBrating", field: "imdbRating"},
-    {headerName: "ID", field: "movieID", hide: true},
-  ]
 
   return (
     <>
-      <Container className="people text-center py-4">
-        <h3>Moviesearch - Person highlight</h3>
-        <div>
-          <h4 className={loading ? "d-block" : "d-none"}></h4>
-          <div className="border-bottom border-4 border-accent">
-            <h1 className="display-1">{person.name}</h1>
-            <h3>{person.birthYear} {person.deathYear ? ` - ${person.deathYear}` : ""}</h3>
-          </div>
-
-          <div className="text-start pt-3">
-            <h4 className="ps-5">Movies</h4>
-            <GridTable 
-              data={movies}
-              columnDefs={columns}
-              onRowClicked={row => navigate(
-                `/movie?movieid=${row.data.movieID}`
-              )}
-            />
-          </div>
-          <div className="text-end pt-3">
-            <h4 className="pe-5">Ratings at a glance</h4>
-            <div className="tempbox">
-              Diagram
-            </div>
-          </div>
-
-        </div>
-      </Container>
+        <h3 className="text-center">Moviesearch - Person highlight</h3>
+        <h4 className={loading ? "d-block" : "d-none"}>Loading ...</h4>
+        <Person {...personDetails} movies = {movies} />
     </>
   )
 };
