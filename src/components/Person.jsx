ddeb4209fc444fad9563736c6a074f9fa7d97f45
haakoaal <haakoaal@stud.ntnu.no>
2025-04-24 21:19:21 +1000

    rating chart

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,6 +1,7 @@
 import { Container } from "reactstrap";
 import GridTable from "../components/GridTable";
 import { useNavigate } from "react-router-dom";
+import Graph from "./Graph";
 
 
 /**
@@ -41,11 +42,9 @@ const Person = ({name, birthYear, deathYear, movies}) => {
               )}
             />
           </div>
-          <div className="text-end pt-3">
-            <h4 className="pe-5">Ratings at a glance</h4>
-            <div className="tempbox">
-              Diagram
-            </div>
+          <div className="py-5">
+            <h4 className="text-end pe-5">Ratings at a glance</h4>
+            <Graph movies={movies}/>
           </div>
         </Container>
     </>
