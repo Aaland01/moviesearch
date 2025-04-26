a42244ec4898a3e878314c6bbf111580fd957f93
haakoaal <haakoaal@stud.ntnu.no>
2025-04-26 16:43:32 +1000

    removed dev button

--- src/components/LoginBody.jsx
+++ src/components/LoginBody.jsx
@@ -80,16 +80,6 @@ const LoginBody = () => {
     clear();
   }
 
-  /**
-   * Development method
-   * Cheatloging to avoid entering details
-   */
-  const cheatLogin = () => {
-    setEmail("mike@gmail.com");
-    setPassword("password")
-    setMessage("Cheater!")
-  };  
-
   return (
     <>
       <Form onSubmit={e => handleSubmit(e)}>
@@ -131,10 +121,6 @@ const LoginBody = () => {
         <ModalFooter>
           <Button color="success" type='submit' disabled={invalidLogin || loading}>Log in</Button>
           <Button color="danger" onClick={cancel}>Cancel</Button>
-          {/*  Cheatlogin for development   */}
-          <div>
-            <Button color="secondary" onClick={cheatLogin}>cheatLogin</Button>
-          </div>
         </ModalFooter>
       </Form>
 
